1efa06b23a27e5805b0e21bec11c9a8fbefb19a2
Alex Anson <1alex.anson@gmail.com>
2021-11-13 16:49:33 -0500

    Writing my first lines of Solidity.

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,25 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = 'Decentragram';
+
+  // Store Images
+  mapping(uint => Image) public images;
+  // 'images' is now a ƒn
+
+  struct Image {
+    uint id; // uint = unsigned integer. non-negative number.
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+  // Create Images
+  function uploadImage() public {
+    // how to add new images to the mapping:
+    images[1] = Image(1, 'abc123', 'Hello, world.', 0, address(0x0));
+  }
+
+  // Tip Images
 }
